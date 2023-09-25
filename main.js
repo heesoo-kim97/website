00a9b11edb26a2c53a0f3cb8cedcf522c14d5955
heesoo-kim97 <heesoo0323@gmail.com>
2023-09-25 06:14:09 +0000

    created layout and event listener.

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-const intro = document.getElementById("intro");
-const web = document.getElementById("web");
-
-console.log(web);
-
-setTimeout(() => {
-  intro.classList.add("hidden");
-}, 8000);
-
-setTimeout(() => {
-  web.classList.remove("hidden");
-}, 5000);
-
-const nav = document.querySelector('.nav')
-window.addEventListener('scroll', fixNav)
-
-function fixNav() {
-  console.log(nav.offsetHeight);
-  if (window.scrollY > nav.offsetHeight * 150) {
-    nav.classList.add('active');
-  } else {
-    nav.classList.remove('active');
-  }
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
+function toggle() {
+  var header = document.getElementById('header');
+  header.classList.toggle('active')
 }
