6d25d05e278a90177230e6a859484c0bb6e9c448
heesoo-kim97 <heesoo0323@gmail.com>
2023-09-25 13:24:20 +0000

    added images and wrapped up projects and contact page.

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ function toggle() {
   var header = document.getElementById('header');
   header.classList.toggle('active')
 }
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = document.getElementById('myForm');
+  const formData = new FormData(form);
+
+  console.log('Form data submitted:', formData);
+
+  form.reset();
+}
+
+const form = document.getElementById('myForm');
+form.addEventListener('submit', handleSubmit);
